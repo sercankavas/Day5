2912de38814f116ee5d88422e6929a6a7cba256c
Sercan Kavas <srsr141986@gmail.com>
2020-10-15 09:41:32 +0200

    feature 3 more commit

--- src/test/java/feature3.java
+++ src/test/java/feature3.java
@@ -1,5 +1,8 @@
 public class feature3 {
     public static void main(String[] args) {
         System.out.println("working on feature 3");
+        System.out.println("more");
+        System.out.println("more");
+        System.out.println("more");
     }
 }
