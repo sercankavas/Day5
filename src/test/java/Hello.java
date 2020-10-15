3282c25d84bc82a9e8bb97676095d02310c128f7
sercankavas <55020291+sercankavas@users.noreply.github.com>
2020-10-15 10:26:50 +0200

    Added changes from github

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello really");
+        System.out.println("Hello really.CHANGED ON THE GITHUB NOW ");
 
     }
 }
