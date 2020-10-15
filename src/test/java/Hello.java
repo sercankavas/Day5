362b8ff844a1c99fcac5b910277ba92390d0161b
Sercan Kavas <srsr141986@gmail.com>
2020-10-15 10:32:40 +0200

    added comments on the same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello really");
+        System.out.println("Hello really");//add one more line on your local and commit
+
 
     }
 }
