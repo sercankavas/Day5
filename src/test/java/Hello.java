84f4928136054741e01f61125f647662591f324f
Sercan Kavas <srsr141986@gmail.com>
2020-10-15 10:46:02 +0200

    Merge branch 'master' of C:\Course\1.week\Day5 with conflicts.

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello really my co-worker");//we are simulating the situation my coworker made this change
+
+        System.out.println("Hello really");//add one more line on your local and commit
+
 
     }
 }
