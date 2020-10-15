f2f5d1b7947a47216993773e9efbafbb796c3784
Sercan Kavas <srsr141986@gmail.com>
2020-10-15 10:53:29 +0200

    adding some local chnges

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello really my co-worker");//we are simulating the situation my coworker made this change!!!
-
+        //adding some changes
     }
 }
